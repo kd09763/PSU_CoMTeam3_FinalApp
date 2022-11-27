2e13b2ef3b8eb3af81d160cd79f3f5536e7e6b84
DanilaBerezin <dxb5608@psu.edu>
2022-11-26 20:32:35 -0500

    Basic outline of home view

---
+++ main/main/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  main
+//
+//  Created by Danny Boy on 11/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ main/main/mainApp.swift
@@ -0,0 +1,17 @@
+//
+//  mainApp.swift
+//  main
+//
+//  Created by Danny Boy on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct mainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
