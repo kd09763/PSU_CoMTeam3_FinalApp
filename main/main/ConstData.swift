76f34dc3e4f002981c668b1b28e0276fc9714c96
Kevin Dong <kd09763@gmail.com>
2022-12-05 08:59:11 -0500

    added all 9 apps

--- main/main/ConstData.swift
+++ main/main/ConstData.swift
@@ -15,6 +15,71 @@ let screen_height = screen_size.height
 // This is where you want to put all the content for the apps (Icon, text descriptions etc.)
 let appInfo : [AppInfo] = [
     AppInfo(
+        icon: "CapMgmtLogo",
+        title: "CapMgmt",
+        leftImage: "rect",
+        rightText: "temp message",
+        rightImage: "rect",
+        leftText: "temp message",
+        url: ""
+    ),AppInfo(
+        icon: "CareLogo",
+        title: "Care Assistant",
+        leftImage: "rect",
+        rightText: "temp message",
+        rightImage: "rect",
+        leftText: "temp message",
+        url: ""
+    ),AppInfo(
+        icon: "FetaLogo",
+        title: "FetaLink +",
+        leftImage: "rect",
+        rightText: "temp message",
+        rightImage: "rect",
+        leftText: "temp message",
+        url: ""
+    ),AppInfo(
+        icon: "MessageLogo",
+        title: "Message Center",
+        leftImage: "rect",
+        rightText: "temp message",
+        rightImage: "rect",
+        leftText: "temp message",
+        url: ""
+    ),AppInfo(
+        icon:"PowerChartLogo",
+        title: "PowerChartTouch",
+        leftImage: "rect",
+        rightText: "temp",
+        rightImage: "rect",
+        leftText: "temp",
+        url: "https://www.childrensmn.org/departments/training/emr/powerchart-touch-user-guide.pdf"
+    ),
+    AppInfo(
+        icon: "PowerMicLogo",
+        title: "PowerMic",
+        leftImage: "rect",
+        rightText: "temp message",
+        rightImage: "rect",
+        leftText: "temp message",
+        url: ""
+    ),AppInfo(
+        icon: "TigerLogo",
+        title: "Tiger Connect",
+        leftImage: "rect",
+        rightText: "temp message",
+        rightImage: "rect",
+        leftText: "temp message",
+        url: ""
+    ),AppInfo(
+        icon: "WorkspaceLogo",
+        title: "Citrix Workspace",
+        leftImage: "rect",
+        rightText: "temp message",
+        rightImage: "rect",
+        leftText: "temp message",
+        url: ""
+    ),AppInfo(
         icon: "ZoomLogo",
         title: "Zoom",
         leftImage: "ZoomHome",
@@ -23,13 +88,4 @@ let appInfo : [AppInfo] = [
         leftText: "With Zoom you can join meeting using the meeting ID given to you and you can cahnge your name as well as pick whether you want to connect your camera or audio.",
         url: "https://support.zoom.us/hc/en-us/articles/206175806-Frequently-asked-questions"
     ),
-    AppInfo(
-        icon:"PowerChartLogo",
-        title: "PowerChart",
-        leftImage: "rect",
-        rightText: "temp",
-        rightImage: "rect",
-        leftText: "temp",
-        url: "https://www.childrensmn.org/departments/training/emr/powerchart-touch-user-guide.pdf"
-    )
 ]
